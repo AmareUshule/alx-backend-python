1295b78177a82876a04246fc8946874609ef2786
Amare Ushule <167173084+AmareUshule@users.noreply.github.com>
2025-11-12 10:00:58 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -51,9 +51,7 @@ class TestGetJson(unittest.TestCase):
     ])
     @patch("utils.requests.get")
     def test_get_json(self, test_url, test_payload, mock_get):
-    """Test that get_json returns correct payload with mocked requests.
-    Uses mocked requests.get to avoid real HTTP calls.
-    """ 
+        """Test that get_json returns correct payload mocked requests.get."""
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         mock_get.return_value = mock_response
