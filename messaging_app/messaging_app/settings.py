ba3afd131ec954f5d6f6394570fa8f3a5ea7b047
Amare Ushule <amirushule@gmail.com>
2025-11-18 09:40:28 +0300

    this updated

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -52,6 +52,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
 
 ROOT_URLCONF = 'messaging_app.urls'
 
