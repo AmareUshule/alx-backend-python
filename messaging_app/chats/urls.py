a49a7d6ffaf10b4e3b9dadad5be058f9f6f59af1
Amare Ushule <amirushule@gmail.com>
2025-11-21 10:11:06 +0300

    Build api endpoints with views

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,3 +1,5 @@
-# Leave this file empty or with a simple comment
-# This file is required by ALX but all routing will be in messaging_app/urls.py
+from django.urls import
+path
+include
+routers.DefaultRouter()
 
